f0e853ad89505c0793305357987cdd8945ca3986
Hanna <anna_kon@bigmir.net>
2023-01-09 23:00:54 +0200

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,24 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryEl = document.querySelector('.gallery');
+
+const galleryItemsMarkup = galleryItems.map(galleryItem => `<a class="gallery__item" href="${galleryItem.original}">
+  <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}" />
+</a>`
+).join('');
+galleryEl.insertAdjacentHTML("beforeend", galleryItemsMarkup);
+
+
+new SimpleLightbox('.gallery a', {
+//   captions: 'true',  
+  captionsData: 'alt',
+    captionDelay: 250,
+    // nav: true,
+    // navText: ['←','→'],
+});
+ 

2f4abdc199008b5b622926c9cc3ae91b61fd6673
Hanna <anna_kon@bigmir.net>
2023-01-10 01:35:11 +0200

    task 2 WIP

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,32 @@
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const TIME_STORAGE_KEY = "videoplayer-current-time";
+
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+function onTimeUpdate(event) {
+    console.dir(event);
+    localStorage.setItem(TIME_STORAGE_KEY, event.seconds);
+    return console.log("currentTime:", localStorage.getItem(TIME_STORAGE_KEY));
+
+}
+
+
+player.setCurrentTime(localStorage.getItem(TIME_STORAGE_KEY) || 0).then(function(seconds) {
+    console.log(seconds);
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            console.log("The time was less than 0 or greater than the video’s duration!");
+            break;
+
+        default:
+            console.log("Some other error occured!");
+            break;
+    }
+});
